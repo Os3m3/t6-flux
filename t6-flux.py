0f93f37210da07e96299abbd3c62e94c9b8bfaa2
Janna-Khalid <janna.balushi@outlook.com>
2025-05-18 11:17:52 +0400

    Decaling the variables and assiging the input

--- t6-flux.py
+++ t6-flux.py
@@ -1,1 +1,8 @@
 team_name = "T6-Flux"
+x = 0
+y = 0
+z = 0
+operation = ""
+x = int(input("Please Enter the 1st Number: "))
+y = int(input("Please Enter the 2st Number: "))
+operation = str(input("Please Enter the Desired Operation: "))
