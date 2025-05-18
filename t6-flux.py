5b97690d6c423761d2136e11033b6e049e3bdbda
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-18 11:31:33 +0400

    I have created the structure of the Proccess

--- t6-flux.py
+++ t6-flux.py
@@ -1,8 +1,22 @@
 team_name = "T6-Flux"
+
+
+#Decleration:
 x = 0
 y = 0
 z = 0
 operation = ""
+
+
+#Input:
+
 x = int(input("Please Enter the 1st Number: "))
 y = int(input("Please Enter the 2st Number: "))
 operation = str(input("Please Enter the Desired Operation: "))
+
+
+#Proccess:
+
+
+
+#Result:
